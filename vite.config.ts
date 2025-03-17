60a72b5c4379a146ff76886a0ab258beca3dd07d
sohailahmed-1469 <suhailahmed.mohammad@gmail.com>
2025-03-17 19:04:24 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react_application/",
+  base: "/react_application/",  // Set this to your GitHub repo name
 });
